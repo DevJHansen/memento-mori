f62140e0327b4b90d99ad821c980a691a25a86a9
Justin Hansen <dev.jhansen@gmail.com>
2024-10-07 15:15:09 +0200

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'storage.googleapis.com'],
   },
+  experimental: {
+    appDir: true,
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
